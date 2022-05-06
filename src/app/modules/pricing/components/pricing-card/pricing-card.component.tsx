d22f9a53c7a1839dfe57e3824213d70ddbe447ca
Nguyen Viet Dai <dai.nguyenviet@vti.com.vn>
2022-05-06 17:38:37 +0700

    continue

--- src/app/modules/pricing/components/pricing-card/pricing-card.component.tsx
+++ src/app/modules/pricing/components/pricing-card/pricing-card.component.tsx
@@ -1,16 +1,65 @@
-import { Card, Typography } from '@mui/material';
-import React from 'react';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  Stack,
+  Typography,
+} from '@mui/material';
+import clsx from 'clsx';
+import React, { useMemo } from 'react';
 import './pricing-card.style.scss';
 
-function PricingCard() {
+interface PricingCardProps {
+  info: PricingCardInfo;
+  isMonthly: boolean;
+}
+
+interface PricingCardInfo {
+  id: number;
+  cardType: number;
+  title: string;
+  subtitle: string;
+  monthlyPrice: number;
+  yearlyPrice: number;
+}
+
+const POPULAR_TYPE = 1;
+
+function PricingCard({ info, isMonthly }: PricingCardProps) {
+  const popularType = info.cardType === POPULAR_TYPE;
+
   return (
     <Card className="pricing-card">
-      <div>
-        <Typography>Essentials</Typography>
-        <Typography>Recommended for 1 to 10 staff</Typography>
+      <div
+        className={clsx('header', {
+          '--popular': popularType,
+        })}
+      >
+        <Stack className="title" direction="row" alignItems="center">
+          <Typography variant="h5">{info.title}</Typography>
+          {popularType && (
+            <Typography className="card-type">Most Popular</Typography>
+          )}
+        </Stack>
+
+        <Typography>{info.subtitle}</Typography>
       </div>
+
+      <CardActionArea className="action-area">
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Typography variant="h4">
+            US${isMonthly ? info.monthlyPrice : info.yearlyPrice}
+            <span>/month</span>
+          </Typography>
+        </Stack>
+
+        <Typography>
+          Engage customers across online touchpoints with rich and contextual
+        </Typography>
+      </CardActionArea>
     </Card>
   );
 }
 
-export default PricingCard;
+export default React.memo(PricingCard);
--- src/app/modules/pricing/pages/pricing-page.component.tsx
+++ src/app/modules/pricing/pages/pricing-page.component.tsx
@@ -1,14 +1,42 @@
 import styled from '@emotion/styled/types/base';
 import { Container, Grid, Paper, Stack, Typography, Box } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import PricingCard from '../components/pricing-card/pricing-card.component';
 import PricingSwitching from '../components/switch-button/switch-button.component';
 import './pricing-page.style.scss';
 
+const fakeData = [
+  {
+    id: 1,
+    cardType: 0,
+    title: 'Essentials',
+    subtitle: 'Recommended for 1 to 10 staff',
+    monthlyPrice: 20,
+    yearlyPrice: 200,
+  },
+  {
+    id: 2,
+    cardType: 1,
+    title: 'Business',
+    subtitle: 'Recommended for more than 10 staff',
+    monthlyPrice: 30,
+    yearlyPrice: 300,
+  },
+  {
+    id: 3,
+    cardType: 0,
+    title: 'Enterprise',
+    subtitle: 'Recommended for more than 20 staff',
+    monthlyPrice: 40,
+    yearlyPrice: 400,
+  },
+];
+
 function Pricing() {
   const { t } = useTranslation();
+  const [isMonthly, setIsMonthly] = useState<boolean>(true);
 
   return (
     <Container maxWidth="xl" className="container mt--XXL">
@@ -38,15 +66,11 @@ function Pricing() {
         </Grid>
 
         <Grid container spacing={3} className="mt--XS pricing-card-list">
-          <Grid item xs={4}>
-            <PricingCard />
-          </Grid>
-          <Grid item xs={4}>
-            <PricingCard />
-          </Grid>
-          <Grid item xs={4}>
-            <PricingCard />
-          </Grid>
+          {fakeData.map((item) => (
+            <Grid item xs={4}>
+              <PricingCard info={item} key={item.id} isMonthly={isMonthly} />
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Container>
